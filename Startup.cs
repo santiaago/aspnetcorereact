50130cf64097f30cd107b8782faac1de45604b43
santiaago <santiago.ariassar@gmail.com>
2017-06-19 21:00:09 +0100

    link app folder as static folder

--- Startup.cs
+++ Startup.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
 
 namespace aspnetcorereact
@@ -36,6 +38,12 @@ namespace aspnetcorereact
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
+            app.UseDefaultFiles();
+            app.UseStaticFiles(new StaticFileOptions(){
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "app", "build")),
+                RequestPath = ""
+                
+            });
 
             app.UseMvc();
         }
