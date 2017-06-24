f6c3b32298ef30b59b47d1ff77cf23a7e45e70ec
santiaago <santiago.ariassar@gmail.com>
2017-06-24 15:26:22 +0100

    get values and display

--- app/src/Values.js
+++ app/src/Values.js
@@ -1,9 +1,30 @@
 import React, {Component} from 'react'
+import $ from 'jquery'
 
 class Values extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            values: []
+        }
+    }
+    getValues(){
+        $.ajax({
+            url: '/api/values',
+            type: "GET",
+            success: function(response){
+                if(response){
+                    this.setState({values: response})
+                }
+            }.bind(this),
+            dataType: 'json'
+	    })
+    }
+
     render(){
         return <div>
-            values here.
+            <div onClick={() => this.getValues()}>get values</div>
+            <div>{this.state.values.map(x => x)}</div>
         </div>
 
     }
